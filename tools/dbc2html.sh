e68632a46bf841224cc493382780b95af9d2aed3
Stéphane Raimbault <stephane.raimbault@polyconseil.fr>
2015-11-06 18:30:45 +0100

    shell script: Better handling of spaces in filename

--- tools/dbc2html.sh
+++ tools/dbc2html.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-BASE=`basename $1 .dbc`
-dbc2json $1 $BASE.json && dbcjson2html.py $BASE.json > $BASE.html && echo "HTML $BASE.html created."
+BASE="`basename $1 .dbc`"
+dbc2json "$1" "$BASE.json" && dbcjson2html.py "$BASE.json" > "$BASE.html" && echo "HTML $BASE.html created."
